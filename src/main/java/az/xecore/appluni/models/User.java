c32726c3bb9db87ab723d971338dfd0a9ff4a2fa
NedNedov228 <nakhmedli@gmail.com>
2025-04-07 22:13:22 +0400

    Connected JPA with Postgres DB

--- src/main/java/az/xecore/appluni/models/User.java
+++ src/main/java/az/xecore/appluni/models/User.java
@@ -1,4 +1,32 @@
 package az.xecore.appluni.models;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.UuidGenerator;
+
+@Entity
+@Table(name = "person")
+@Getter
+@Setter
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_DEFAULT)
 public class User {
+
+    @Id
+    @UuidGenerator
+    @Column(name = "id",unique = true,updatable = false)
+    private String id;
+
+    private String firstName;
+    private String lastName;
+
+    private String email;
+    private String username;
+
 }
