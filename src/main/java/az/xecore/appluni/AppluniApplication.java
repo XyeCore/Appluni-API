6452fd617e779627f333fc2aff17a40ebfa6c910
NiCat Ahmedov <nakhmedli@gmail.com>
2025-04-07 17:29:15 +0200

    first commit

---
+++ src/main/java/az/xecore/appluni/AppluniApplication.java
@@ -0,0 +1,13 @@
+package az.xecore.appluni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppluniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppluniApplication.class, args);
+    }
+
+}
---
+++ src/test/java/az/xecore/appluni/AppluniApplicationTests.java
@@ -0,0 +1,13 @@
+package az.xecore.appluni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppluniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
