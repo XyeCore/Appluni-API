5635c97bde99d1cca2933ddd7562bdb62bba46e2
NedNedov228 <nakhmedli@gmail.com>
2025-05-11 18:11:08 +0400

    Configured env for application.properties

--- src/main/java/az/xecore/appluni/AppluniApplication.java
+++ src/main/java/az/xecore/appluni/AppluniApplication.java
@@ -12,15 +12,17 @@ public class AppluniApplication {
 
     public static void main(String[] args) {
 
-        Dotenv dotenv = Dotenv.configure().load();
 
-        // Pass env-s in System Properties
-        System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
-        System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
-        System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
-        System.setProperty("TOKEN_SIGNING_KEY", dotenv.get("TOKEN_SIGNING_KEY"));
-
-        // Test logs
+//        Get vars from .env TODO: check .env if file exists
+//        Dotenv dotenv = Dotenv.configure().load();
+//
+//        // Pass env-s in System Properties
+//        System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
+//        System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+//        System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+//        System.setProperty("TOKEN_SIGNING_KEY", dotenv.get("TOKEN_SIGNING_KEY"));
+//
+//        // Test logs
         System.out.println("DB URL: " + System.getProperty("SPRING_DATASOURCE_URL"));
         System.out.println("DB USERNAME: " + System.getProperty("SPRING_DATASOURCE_USERNAME"));
 
