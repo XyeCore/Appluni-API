2fa8d40d24d57554e4c70b5f09d42c8194bdd6f7
NedNedov228 <nakhmedli@gmail.com>
2025-05-11 17:42:30 +0400

    Configured .env for application.properties

--- src/main/java/az/xecore/appluni/AppluniApplication.java
+++ src/main/java/az/xecore/appluni/AppluniApplication.java
@@ -14,14 +14,17 @@ public class AppluniApplication {
 
         Dotenv dotenv = Dotenv.configure().load();
 
-        // Передаем переменные окружения в System Properties
+        // Pass env-s in System Properties
         System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
         System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
         System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
         System.setProperty("TOKEN_SIGNING_KEY", dotenv.get("TOKEN_SIGNING_KEY"));
 
-        // Проверка
+        // Test logs
         System.out.println("DB URL: " + System.getProperty("SPRING_DATASOURCE_URL"));
+        System.out.println("DB USERNAME: " + System.getProperty("SPRING_DATASOURCE_USERNAME"));
+
+
 
         SpringApplication.run(AppluniApplication.class, args);
     }
